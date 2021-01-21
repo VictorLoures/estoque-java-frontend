28c8765862ea132dfc993b66b0d53f5dd10b3faf
Victor Loures <victorloures10@gmail.com>
2021-01-21 16:10:08 -0300

    Buscando as categorias no backend e mostrando

---
+++ src/config/api.config.ts
@@ -0,0 +1,4 @@
+export const API_CONFIG = {
+    baseurl: "http://localhost:8080",
+    bucketUrl: "assets/imgs/"
+}
---
+++ src/pages/categorias/categorias.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaService } from '../../domain/categoria.service';
+import { CategoriaDTO } from '../../models/CategoriaDTO';
+
+
+/**
+ * Generated class for the CategoriasPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-categorias',
+  templateUrl: 'categorias.html',
+})
+export class CategoriasPage {
+
+  bucketUrl: string = API_CONFIG.bucketUrl;
+
+  items: CategoriaDTO[];
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService : CategoriaService) {
+  }
+
+  ionViewDidLoad() {
+    this.categoriaService.findAll().subscribe(response => {this.items = response});
+  }
+
+}
